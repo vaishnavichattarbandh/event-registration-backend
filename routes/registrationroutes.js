b140577087883deffc36b45ddc663e1401d22c3b
vaishnavichattarbandh <vaishnavichattarbandh@gmail.com>
2026-01-17 09:55:55 +0530

    Fix event registrations API

--- routes/registrationroutes.js
+++ routes/registrationroutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Registration = require("../models/Registration");
+const Registration = require("../models/registration");
 
 // Register for an event
 router.post("/", async (req, res) => {
